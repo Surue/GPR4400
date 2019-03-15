5a7c7284e3c06b111ef0d99ecb299c397026c2bc
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-03-15 15:18:11 +0100

    Fix flood algorithm

---
+++ Assets/Maze/Scripts/Chunck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunck : MonoBehaviour
+{
+
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        if (up) {
+            Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y + 2.5f), 1);
+        }
+
+        if(down) {
+            Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y - 2.5f), 1);
+        }
+
+        if(left) {
+            Gizmos.DrawWireSphere(new Vector3(transform.position.x - 2.5f, transform.position.y), 1);
+        }
+
+        if(right) {
+            Gizmos.DrawWireSphere(new Vector3(transform.position.x + 2.5f, transform.position.y), 1);
+        }
+    }
+}
--- Assets/Maze/Scripts/Mazegenerator.cs
+++ Assets/Maze/Scripts/Mazegenerator.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
+    [SerializeField] int cellNbX = 2;
+    [SerializeField] int cellNbY = 7;
+
+    int sizeX = 5;
+    int sizeY = 5;
+
+    struct Cell
+    {
+        float posX;
+        float posY;
+
+        Chunck chunk;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +28,14 @@ public class MazeGenerator : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnDrawGizmos()
+    {
+        for (int x = 0; x < cellNbX; x++) {
+            for (int y = 0; y < cellNbY; y++) {
+                Gizmos.DrawWireCube(new Vector3(x * sizeX + sizeX / 2f, y * sizeY + sizeY / 2f), new Vector3(sizeX, sizeY));
+            }
+        }
     }
 }
