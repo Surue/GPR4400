eb6bc3aae6e463100c507475a4564353301076a1
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-03-20 21:51:49 +0100

    Seek behavior

---
+++ Assets/SteeringBehavior/Scripts/Behaviors/SeekBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekBehavior : SteeringBehavior
+{
+    public override Vector2 Steer(Rigidbody2D body, Collider2D col, float maxSpeed)
+    {
+        Vector2 seekVelocity = Vector2.zero;
+        
+        if(col.gameObject.CompareTag("Attractor")) {
+            seekVelocity = col.transform.position - body.transform.position;
+            seekVelocity = seekVelocity.normalized * maxSpeed;
+            seekVelocity += seekVelocity - body.velocity;
+        }
+
+        return seekVelocity;
+    }
+}
