e65f7aa9eec92966dfacc2407865ccc9c092d086
Nicolas Schneider <nicolas06062000@hotmail.com>
2019-03-22 16:24:38 +0100

    Add chunk based generation

--- Assets/Maze/Scripts/Mazegenerator.cs
+++ Assets/Maze/Scripts/Mazegenerator.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using UnityScript.Steps;
 using Random = UnityEngine.Random;
 
 public class MazeGenerator : MonoBehaviour
@@ -27,22 +23,19 @@ public class MazeGenerator : MonoBehaviour
 
     Cell[,] cells;
 
-    enum Direction
+    enum RuleState
     {
-        UP = 0x0,
-        DOWN = 0x1,
-        LEFT,
-        RIGHT
+        ALWAYS_TRUE,
+        ALWAYS_FALSE,
+        DONT_CARE
     }
 
     struct Rule
     {
-        public bool up;
-        public bool down;
-        public bool left;
-        public bool right;
-
-        public Direction parentDirection;
+        public RuleState up;
+        public RuleState down;
+        public RuleState left;
+        public RuleState right;
     }
 
     // Start is called before the first frame update
@@ -94,108 +87,132 @@ public class MazeGenerator : MonoBehaviour
 
     void GenerateLevel()
     {
-        int flag = 0;
-
-        flag = 1 << 1;
-        flag = 1 << 2;
-
-        switch (flag) {
-            case 0x0000:
-                break;
-        }
     }
+
+    int step = 10;
 
     void GenerateChunkChild(Cell cell)
     {
-        //Parcourir tout les portes possible
-        if (cell.chunk.down) {
+        step--;
 
+        if (step < 0) {
+            return;
         }
 
+        if (cell.chunk.down && cells[cell.x, cell.y - 1].chunk == null) {
+            Rule rule = GetRulesForNextChunk(cell.x, cell.y - 1);
 
-        if (cell.chunk.up) {
-            Rule rule = CheckNewCellUp(cell.x, cell.y + 1);
+            GameObject newChunk = SelectChunk(rule);
 
-            GameObject newChunk = GameObject.Instantiate(SelectChunk(rule));
-            newChunk.name = "Chunk";
-            newChunk.transform.position = new Vector2(sizeX * (cell.x) + sizeX / 2f, sizeY *(cell.y + 1) + sizeY / 2f);
+            if (newChunk != null) {
+                GameObject instance = GameObject.Instantiate(SelectChunk(rule));
 
-            cells[cell.x, cell.y + 1].chunk = newChunk.GetComponent<Chunck>();
+                instance.name = "Chunk (" + cell.x.ToString() + ", " + (cell.y - 1).ToString() + ")";
+                instance.transform.position =
+                    new Vector2(sizeX * (cell.x) + sizeX / 2f, sizeY * (cell.y - 1) + sizeY / 2f);
+
+                cells[cell.x, cell.y - 1].chunk = instance.GetComponent<Chunck>();
+
+                GenerateChunkChild(cells[cell.x, cell.y - 1]);
+            }
         }
 
-        if (cell.chunk.left) {
+        if (cell.chunk.up && cells[cell.x, cell.y + 1].chunk == null) {
+            Rule rule = GetRulesForNextChunk(cell.x, cell.y + 1);
 
+            GameObject newChunk = SelectChunk(rule);
+
+            if (newChunk != null) {
+                GameObject instance = GameObject.Instantiate(SelectChunk(rule));
+                instance.name = "Chunk (" + cell.x.ToString() + ", " + (cell.y + 1).ToString() + ")";
+                instance.transform.position =
+                    new Vector2(sizeX * (cell.x) + sizeX / 2f, sizeY * (cell.y + 1) + sizeY / 2f);
+
+                cells[cell.x, cell.y + 1].chunk = instance.GetComponent<Chunck>();
+
+                GenerateChunkChild(cells[cell.x, cell.y + 1]);
+            }
         }
 
-        if(cell.chunk.right) {
-            Rule rule = CheckNewCellRight(cell.x + 1, cell.y);
+        if (cell.chunk.left && cells[cell.x - 1, cell.y].chunk == null) {
+            Rule rule = GetRulesForNextChunk(cell.x - 1, cell.y);
 
-            GameObject newChunk = GameObject.Instantiate(SelectChunk(rule));
-            newChunk.name = "Chunk";
-            newChunk.transform.position = new Vector2(sizeX * (cell.x + 1) + sizeX / 2f, sizeY * (cell.y) + sizeY / 2f);
+            GameObject newChunk = SelectChunk(rule);
 
-            cells[cell.x + 1, cell.y].chunk = newChunk.GetComponent<Chunck>();
+            if (newChunk != null) {
+                GameObject instance = GameObject.Instantiate(SelectChunk(rule));
+                instance.name = "Chunk (" + (cell.x - 1).ToString() + ", " + (cell.y).ToString() + ")";
+                instance.transform.position =
+                    new Vector2(sizeX * (cell.x - 1) + sizeX / 2f, sizeY * (cell.y) + sizeY / 2f);
+
+                cells[cell.x - 1, cell.y].chunk = instance.GetComponent<Chunck>();
+
+                GenerateChunkChild(cells[cell.x - 1, cell.y]);
+            }
+        }
+
+        if (cell.chunk.right && cells[cell.x + 1, cell.y].chunk == null) {
+            Rule rule = GetRulesForNextChunk(cell.x + 1, cell.y);
+
+            GameObject newChunk = SelectChunk(rule);
+
+            if (newChunk != null) {
+                GameObject instance = GameObject.Instantiate(SelectChunk(rule));
+                instance.name = "Chunk (" + (cell.x + 1).ToString() + ", " + (cell.y).ToString() + ")";
+                instance.transform.position =
+                    new Vector2(sizeX * (cell.x + 1) + sizeX / 2f, sizeY * (cell.y) + sizeY / 2f);
+
+                cells[cell.x + 1, cell.y].chunk = instance.GetComponent<Chunck>();
+
+                GenerateChunkChild(cells[cell.x + 1, cell.y]);
+            }
         }
 
         //Pour chaque porte on vérifie les voisins
     }
 
-    Rule CheckNewCellUp(int x, int y)
+    Rule GetRulesForNextChunk(int x, int y)
     {
         Rule rule;
-
-        rule.down = true;
-        rule.parentDirection = Direction.DOWN;
-
-        //left
-        if (x > 0 && cells[x - 1, y].chunk == null) {
-            rule.left = true;
-        } else {
-            rule.left = false;
-        }
-
-        //right
-        if(x < cellNbX && cells[x + 1, y].chunk == null) {
-            rule.right = true;
-        } else {
-            rule.right = false;
-        }
-
-        //up
-        if(y < cellNbY && cells[x, y + 1].chunk == null) {
-            rule.up = true;
-        } else {
-            rule.up = false;
-        }
-
-        return rule;
-    }
-
-    Rule CheckNewCellRight(int x, int y) {
-        Rule rule;
-
-        rule.left = true;
-        rule.parentDirection = Direction.LEFT;
+        rule.up = RuleState.DONT_CARE;
+        rule.left = RuleState.DONT_CARE;
+        rule.right = RuleState.DONT_CARE;
+        rule.down = RuleState.DONT_CARE;
 
         //down
-        if(y > 0 && cells[x, y - 1].chunk == null) {
-            rule.down = true;
+        if (y > 0) {
+            if (cells[x, y - 1].chunk != null) {
+                rule.down = cells[x, y - 1].chunk.up ? RuleState.ALWAYS_TRUE : RuleState.ALWAYS_FALSE;
+            }
         } else {
-            rule.down = false;
+            rule.down = RuleState.ALWAYS_FALSE;
+        }
+
+        //left
+        if (x > 0) {
+            if (cells[x - 1, y].chunk != null) {
+                rule.left = cells[x - 1, y].chunk.right ? RuleState.ALWAYS_TRUE : RuleState.ALWAYS_FALSE;
+            }
+        } else {
+            rule.left = RuleState.ALWAYS_FALSE;
         }
 
         //right
-        if(x < cellNbX && cells[x + 1, y].chunk == null) {
-            rule.right = true;
+        if (x < cellNbX - 1) {
+            if (cells[x + 1, y].chunk != null) {
+                rule.right = cells[x + 1, y].chunk.left ? RuleState.ALWAYS_TRUE : RuleState.ALWAYS_FALSE;
+            }
         } else {
-            rule.right = false;
+            rule.right = RuleState.ALWAYS_FALSE;
         }
 
         //up
-        if(y < cellNbY && cells[x, y + 1].chunk == null) {
-            rule.up = true;
+        if (y < cellNbY - 1) {
+            if (cells[x, y + 1].chunk != null) {
+                rule.up = cells[x, y + 1].chunk.down ? RuleState.ALWAYS_TRUE : RuleState.ALWAYS_FALSE;
+            }
         } else {
-            rule.up = false;
+            rule.up = RuleState.ALWAYS_FALSE;
         }
 
         return rule;
@@ -203,155 +220,32 @@ public class MazeGenerator : MonoBehaviour
 
     GameObject SelectChunk(Rule rule)
     {
-        List<GameObject> possibleNewChunk = new List<GameObject>();
-        for(int i = 0;i < chunksAvailable.Length;i++) {
-            Chunck currentChunk = chunksAvailable[i].GetComponent<Chunck>();
+        List<GameObject> possibleNewChunk = (from t in chunksAvailable
+            let currentChunk = t.GetComponent<Chunck>()
+            where rule.down != RuleState.ALWAYS_TRUE || currentChunk.down
+            where rule.down != RuleState.ALWAYS_FALSE || !currentChunk.down
+            where rule.up != RuleState.ALWAYS_TRUE || currentChunk.up
+            where rule.up != RuleState.ALWAYS_FALSE || !currentChunk.up
+            where rule.left != RuleState.ALWAYS_TRUE || currentChunk.left
+            where rule.left != RuleState.ALWAYS_FALSE || !currentChunk.left
+            where rule.right != RuleState.ALWAYS_TRUE || currentChunk.right
+            where rule.right != RuleState.ALWAYS_FALSE || !currentChunk.right
+            select t).ToList();
 
-            switch (rule.parentDirection) {
-                case Direction.UP:
-                    if(!currentChunk.up) continue;
-
-                    //100
-                    if (rule.down && !rule.left && !rule.right) {
-                        if (currentChunk.down && !currentChunk.left && !currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //110
-                    if(rule.down && rule.left && !rule.right) {
-                        if(currentChunk.down && currentChunk.left && !currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //111
-                    if(rule.down && rule.left && rule.right) {
-                        if(currentChunk.down && currentChunk.left && currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //101
-                    if(rule.down && !rule.left && rule.right) {
-                        if(currentChunk.down && !currentChunk.left && currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //011
-                    if(!rule.down && rule.left && rule.right) {
-                        if(!currentChunk.down && currentChunk.left && currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //001
-                    if(!rule.down && !rule.left && rule.right) {
-                        if(!currentChunk.down && !currentChunk.left && !currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //000
-                    if(!rule.down && !rule.left && !rule.right) {
-                        if(!currentChunk.down && !currentChunk.left && !currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //010
-                    if(!rule.down && rule.left && !rule.right) {
-                        if(!currentChunk.down && currentChunk.left && !currentChunk.right) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-                    
-                    break;
-                case Direction.DOWN:
-                    if(!currentChunk.down) continue;
-                    break;
-                case Direction.LEFT:
-                    if(!currentChunk.left) continue;
-                    break;
-                case Direction.RIGHT:
-                    if(!currentChunk.right) continue;
-
-                    //100
-                    if(rule.down && !rule.left && !rule.up) {
-                        if(currentChunk.down && !currentChunk.left && !currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //110
-                    if(rule.down && rule.left && !rule.up) {
-                        if(currentChunk.down && currentChunk.left && !currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //111
-                    if(rule.down && rule.left && rule.up) {
-                        if(currentChunk.down && currentChunk.left && currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //101
-                    if(rule.down && !rule.left && rule.up) {
-                        if(currentChunk.down && !currentChunk.left && currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //011
-                    if(!rule.down && rule.left && rule.up) {
-                        if(!currentChunk.down && currentChunk.left && currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //001
-                    if(!rule.down && !rule.left && rule.up) {
-                        if(!currentChunk.down && !currentChunk.left && !currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //000
-                    if(!rule.down && !rule.left && !rule.up) {
-                        if(!currentChunk.down && !currentChunk.left && !currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-
-                    //010
-                    if(!rule.down && rule.left && !rule.up) {
-                        if(!currentChunk.down && currentChunk.left && !currentChunk.up) {
-                            possibleNewChunk.Add(chunksAvailable[i]);
-                        }
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        return possibleNewChunk[Random.Range(0, possibleNewChunk.Count)];
+        return possibleNewChunk.Count == 0 ? null : possibleNewChunk[Random.Range(0, possibleNewChunk.Count)];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnDrawGizmos()
     {
         for (int x = 0; x < cellNbX; x++) {
             for (int y = 0; y < cellNbY; y++) {
-                Gizmos.DrawWireCube(new Vector3(x * sizeX + sizeX / 2f, y * sizeY + sizeY / 2f), new Vector3(sizeX, sizeY));
+                Gizmos.DrawWireCube(new Vector3(x * sizeX + sizeX / 2f, y * sizeY + sizeY / 2f),
+                    new Vector3(sizeX, sizeY));
             }
         }
     }
